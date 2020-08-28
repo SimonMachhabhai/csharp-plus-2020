6e0925783a54add46c612b238e0b6e9b93f68697
SimonMachhabhai <Simon_96m@hotmail.com>
2020-08-28 13:08:28 +0200

    Progress on number app

---
+++ Övning/Övning/Övning/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace Övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in värden: ");
+
+            var input = Console.ReadLine();
+            var inputArray = input.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            double[] numberArray = new double[inputArray.Length];
+
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                NumberStyles numberStyle = NumberStyles.Integer | NumberStyles.Float;
+
+                bool parsed = double.TryParse(inputArray[i], numberStyle, CultureInfo, InvariantCulture, out)
+
+                if (parsed)
+                {
+                    numerArra
+                }
+
+
+
+                
+
+                
+
+                //try
+                //{
+                //    numberArray[i] = Convert.ToDouble(inputArray[i]);
+
+
+                    
+
+                //}
+                //catch (Exception)
+                //{
+
+                //    numberArray[i] = 0;
+
+                //}
+
+            }
+
+
+        }
+
+    }
+}
